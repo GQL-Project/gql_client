1108b828531cf83471197e675d1c8a56d1ce8daf
Ryan Hawks <rhawks@purdue.edu>
2022-09-18 16:02:38 -0400

    Added Test Case for Send Query Page
    Added a test case for the send query page

--- tests/suite_GUI_Tests/shared/scripts/names.py
+++ tests/suite_GUI_Tests/shared/scripts/names.py
@@ -3,3 +3,7 @@
 from objectmaphelper import *
 
 mainWindow_MainWindow = {"name": "MainWindow", "type": "MainWindow", "visible": 1}
+mainWindow_changePageButton_QPushButton = {"name": "changePageButton", "type": "QPushButton", "visible": 1, "window": mainWindow_MainWindow}
+sendquerypage_sendquerypage = {"name": "sendquerypage", "occurrence": 2, "type": "sendquerypage", "visible": 1}
+sendquerypage_lineEdit_QLineEdit = {"name": "lineEdit", "type": "QLineEdit", "visible": 1, "window": sendquerypage_sendquerypage}
+sendquerypage_pushButton_QPushButton = {"name": "pushButton", "type": "QPushButton", "visible": 1, "window": sendquerypage_sendquerypage}
