eb1b56fe84b64a47bdbcc71174ea4a9f447527c1
Ryan Hawks <rhawks@purdue.edu>
2022-09-22 19:53:42 -0400

    Adjusted Query Textbox

--- tests/suite_GUI_Tests/shared/scripts/names.py
+++ tests/suite_GUI_Tests/shared/scripts/names.py
@@ -7,3 +7,4 @@ mainWindow_changePageButton_QPushButton = {"name": "changePageButton", "type": "
 sendquerypage_sendquerypage = {"name": "sendquerypage", "occurrence": 2, "type": "sendquerypage", "visible": 1}
 sendquerypage_lineEdit_QLineEdit = {"name": "lineEdit", "type": "QLineEdit", "visible": 1, "window": sendquerypage_sendquerypage}
 sendquerypage_pushButton_QPushButton = {"name": "pushButton", "type": "QPushButton", "visible": 1, "window": sendquerypage_sendquerypage}
+mainWindow_plainTextEdit_QPlainTextEdit = {"name": "plainTextEdit", "occurrence": 2, "type": "QPlainTextEdit", "visible": 1, "window": mainWindow_MainWindow}
