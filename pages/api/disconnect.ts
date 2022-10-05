0172ae32acbdba886165537276c8df6b74f84517
Shayne Marques <marques.shayne24@gmail.com>
2022-10-05 04:40:46 -0400

    Updated api to retrieve id from req body and removed cookie funcs & uses

--- pages/api/disconnect.ts
+++ pages/api/disconnect.ts
@@ -1,29 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { Empty } from '../../google/protobuf/empty'
-import { connection, delCookie, getCookie } from './connect'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Empty } from "../../google/protobuf/empty";
+import { connection } from "./connect";
 
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const id: string = JSON.parse(req.body)["id"] as unknown as string;
   console.log("Disconnect Request Received.");
-  if (getCookie(req)) {
-    connection().DisconnectDB({
-      id: getCookie(req)!
-    }, function (err: Error | null, response: Empty) {
-      delCookie(res);
+  connection().DisconnectDB(
+    {
+      id: id,
+    },
+    function (err: Error | null, response: Empty) {
       if (err) {
         console.log(err);
-        res.status(500).end('DisconnectDB: Unable to connect to database');
+        res.status(500).end("DisconnectDB: Unable to connect to database");
         return;
       }
       console.log("DisconnectDB: Disconnected from Database!: ", response);
       res.status(200).json({});
-    });
-  } else {
-    console.log("DisconnectDB: Cookie does not exist");
-    res.status(404).end('DisconnectDB: Unable to Disconnect, not connected to database');
-  }
+    }
+  );
 }
--- pages/api/vcs.ts
+++ pages/api/vcs.ts
@@ -1,30 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { UpdateResult } from '../proto/connection';
-import { connection, getCookie } from './connect'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { UpdateResult } from "../proto/connection";
+import { connection } from "./connect";
 
-
-export default function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
-) {
-    const query: string = JSON.parse(req.body)['query'] as unknown as string;
-    console.log("VC Request Received: " + query);
-    if (getCookie(req)) {
-        connection().RunVersionControlCommand({
-            id: getCookie(req)!,
-            query: query
-        }, function (err: Error | null, response: UpdateResult) {
-            if (err) {
-                console.log(err);
-                res.status(500).end('RunVCS: Error running query');
-                return;
-            }
-            console.log("RunVCS: Update Returned!: ", response);
-            res.status(200).json(response);
-        });
-    } else {
-        console.log("RunVCS: Cookie does not exist");
-        res.status(404).end('RunVCS: Unable to find User');
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const query: string = JSON.parse(req.body)["query"] as unknown as string;
+  const id: string = JSON.parse(req.body)["id"] as unknown as string;
+  console.log("VC Request Received: " + query);
+  connection().RunVersionControlCommand(
+    {
+      id: id,
+      query: query,
+    },
+    function (err: Error | null, response: UpdateResult) {
+      if (err) {
+        console.log(err);
+        res.status(500).end("RunVCS: Error running query");
+        return;
+      }
+      console.log("RunVCS: Update Returned!: ", response);
+      res.status(200).json(response);
     }
+  );
 }
