e0e479c0233a7e6a207dbaa2b863446eac7bbc0e
Ryan Hawks <rhawks@purdue.edu>
2022-12-07 12:54:12 -0500

    Finished Order By Date
    Ordery By Date Works now, still trying to fix a bug for sort by person

--- pages/log.tsx
+++ pages/log.tsx
@@ -25,12 +25,18 @@ import en from "javascript-time-ago/locale/en";
 function Log() {
   const authContext = useContext(AuthContext);
   const [logList, setLogList] = useState({});
+  const [tempLogList, setTempLogList] = useState({});
   const [error, setError] = useState("No Commits");
   const [filterSelection, setFilterSelection] = useState("My Commits")
-  const [newToOld, setNewToOld] = useState(true)
+  const [newToOld, setNewToOld] = useState("NewToOld");
+  const [text, setText] = useState("");
+  const [personBool, setPersonBool] = useState(false);
+  const [defaultText, setDefaultText] = useState("Enter a username");
+  const [personPressed, setPersonPressed] = useState(false);
+  const [masterList, setMasterList] = useState({});
   TimeAgo.addDefaultLocale(en);
   const timeAgo = new TimeAgo("en-US");
-  
+
 
   function groupArrayOfObjects(list, key) {
     return list.reduce(function (rv, x) {
@@ -44,166 +50,225 @@ function Log() {
     router.push("/editor");
   };
 
-  const handleFilterChange = (event: SelectChangeEvent) => {
-    setFilterSelection(event.target.value);
-    var selection = event.target.value;
-    var temp = [];
-    
-    if (selection == "My Commits") {
-      // TODO Sorting Logic for my commits
-      console.log("Testing my commits");
-      
-    } else if (selection == "Newest Commits"){
-      // TODO Sort to have newest commits first\
-      console.log("Testing newest commits");
-      if (!newToOld) {
-        for (var key in logList) {
-          temp.push(logList[key]);
-        }
-        temp.reverse();
-        setLogList(temp);
-        console.log(temp);
-        setNewToOld(true);
-      }
-      for (var key in temp) {
-        console.log(key);
-      }
-      //setLogList(logList.reverse());
-    } else if (selection == "Oldest Commits"){
-      // TODO Sort by the oldest commits first
-      console.log("Testing oldest commits");
-      for (var key in logList) {
-        console.log(logList[key][0].timestamp);
-      }
-      if (newToOld) {
-        for (var key in logList) {
-          temp.push(logList[key]);
-        }
-        temp.reverse();
-        setLogList(temp);
-        console.log(temp);
-        setNewToOld(false);
-      }
+  const handleTextChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(event.target.value);
+    for (var key in logList) {
+      console.log(key);
     }
   };
 
-  useEffect(() => {
-    if (window.localStorage.getItem("loggedIn") !== null) {
-      authContext.login(window.localStorage.getItem("loggedIn"));
-    }
-    async function getLogs() {
-      const response = await fetch("/api/vcs", {
-        method: "POST",
-        body: JSON.stringify({
-          query: "gql log -j",
-          id: authContext.loggedIn,
-        }),
-      });
-      if (!response.ok) {
-        setLogList([]);
-        setError("Error Retrieving Log!");
-      }
-      const data: UpdateResult = await response.json();
-      if (data.message !== "") {
-        if (data.message.toLowerCase() === "no commits!") {
-          setLogList([]);
-        } else {
-          const logs: [] = JSON.parse(data.message).map((log) => {
-            const newLog = {
-              userid: log.user_id,
-              message: log.message,
-              hash: log.hash.slice(0, 7),
-              timestamp: new Date(+log.timestamp),//.toDateString(),
-              timeAgo: timeAgo.format(+log.timestamp),
-            };
-            return newLog;
-          });
-          setLogList(groupArrayOfObjects(logs, "timestamp"));
-          console.log(groupArrayOfObjects(logs, "timestamp"));
-          //setLogList(logs);
+    const handlePerson = () => {
+      setTempLogList(logList);
+      var temp = [];
+      for (var key in logList) {
+        if (logList[key][0].userid == text) {
+          temp.push(logList[key]);
         }
       }
+      if (temp.length == 0) {
+        setDefaultText("No commits found for this user");
+        setText("");
+      } else {
+        setLogList(temp);
+      }
+      console.log("Log list " + logList);
+      console.log("TEMP Log list " + tempLogList)
+      setPersonPressed(true);
     }
-    getLogs();
-  }, [authContext.loggedIn]);
-  return (
-    authContext.loggedIn && (
-      <Box className={`${styles.background}`}>
-        <Head>
-          <title>GQL Editor</title>
-        </Head>
-        <Button
-          className={styles.button}
-          sx={{
-            color: "white",
-            marginTop: "1vh",
-            height: "10vh",
-            width: "20vw",
-            margin: "2vw",
-            fontSize: "larger",
-            backgroundColor: "rgba(34, 34, 34, 0.438)",
-          }}
-          onClick={handleBack}
-        >{`← Back`}</Button>
-        <Box className={styles.center}>
-          <h1>Database Log</h1>
-        </Box>
-       {/* {logList.length > 0 ?*/} 
-        <h2 className={styles.center}>Order By:
-          <Select
-            defaultValue="Newest Commits"
-            style={{ marginLeft: '.5rem' }}
-            onChange={handleFilterChange}
-          >
-            <MenuItem value="Newest Commits">Newest Commits</MenuItem>
-            <MenuItem value="Oldest Commits">Oldest Commits</MenuItem>
-            <MenuItem value="My Commits">My Commits</MenuItem>
-          </Select>
+
+    const handleFilterChange = async (event: SelectChangeEvent) => {
+
+      setFilterSelection(event.target.value);
+      var selection = event.target.value;
+      var temp = [];
+
+      if (selection == "Person") {
+        // TODO Sorting Logic for my commits
+        setPersonBool(true);
+        //setTempLogList(logList);
+      } else if (selection == "Newest Commits") {
+        if (personPressed) {
+          setLogList(masterList);
+          setPersonPressed(false);
+        }
+        // TODO Sort to have newest commits first\
+        setPersonBool(false);
+        setText("");
+        setDefaultText("Enter a username");
+
+        if (newToOld == "OldToNew") {
+          for (var key in logList) {
+            temp.push(logList[key]);
+          }
+          temp.reverse();
+          setLogList(temp);
+          setTempLogList(temp);
+          setMasterList(temp);
+          setNewToOld("NewToOld");
+          
+        }
+        
+      } else if (selection == "Oldest Commits") {
+        // TODO Sort by the oldest commits first
+        if (personPressed) {
+          setLogList(masterList);
+          setPersonPressed(false);
+        }
+        setPersonBool(false);
+        setText("");
+        setDefaultText("Enter a username");
+        if (newToOld == "NewToOld") {
+          for (var key in logList) {
+            temp.push(logList[key]);
+          }
+          temp.reverse();
+          setLogList(temp);
+          setTempLogList(temp);
+          setMasterList(temp);
+          setNewToOld("OldToNew");
+        }
+      }
+    };
+
+    useEffect(() => {
+      if (window.localStorage.getItem("loggedIn") !== null) {
+        authContext.login(window.localStorage.getItem("loggedIn"));
+      }
+      async function getLogs() {
+        const response = await fetch("/api/vcs", {
+          method: "POST",
+          body: JSON.stringify({
+            query: "gql log -j",
+            id: authContext.loggedIn,
+          }),
+        });
+        if (!response.ok) {
+          setLogList([]);
+          setError("Error Retrieving Log!");
+        }
+        const data: UpdateResult = await response.json();
+        if (data.message !== "") {
+          if (data.message.toLowerCase() === "no commits!") {
+            setLogList([]);
+          } else {
+            const logs: [] = JSON.parse(data.message).map((log) => {
+              const newLog = {
+                userid: log.user_id,
+                message: log.message,
+                hash: log.hash.slice(0, 7),
+                timestamp: new Date(+log.timestamp),//.toDateString(),
+                timestamp2: new Date(+log.timestamp).toDateString(),
+                timeAgo: timeAgo.format(+log.timestamp),
+              };
+              return newLog;
+            });
+            setLogList(groupArrayOfObjects(logs, "timestamp"));
+            setTempLogList(logList);
+          }
+        }
+      }
+      getLogs();
+    }, [authContext.loggedIn]);
+    return (
+      authContext.loggedIn && (
+        <Box className={`${styles.background}`}>
+          <Head>
+            <title>GQL Editor</title>
+          </Head>
+          <Button
+            className={styles.button}
+            sx={{
+              color: "white",
+              marginTop: "1vh",
+              height: "10vh",
+              width: "20vw",
+              margin: "2vw",
+              fontSize: "larger",
+              backgroundColor: "rgba(34, 34, 34, 0.438)",
+            }}
+            onClick={handleBack}
+          >{`← Back`}</Button>
+          <Box className={styles.center}>
+            <h1>Database Log</h1>
+          </Box>
+          {/* {logList.length > 0 ?*/}
+          <h2 className={styles.center}>Sort By:
+            <Select
+              defaultValue="Newest Commits"
+              style={{ marginLeft: '.5rem', marginTop: '.1rem' }}
+              onChange={handleFilterChange}
+            >
+              <MenuItem value="Newest Commits">Newest Commits</MenuItem>
+              <MenuItem value="Oldest Commits">Oldest Commits</MenuItem>
+              <MenuItem value="Person">Person</MenuItem>
+            </Select>
+            {personBool ? <><h5>Enter Person to Order By:</h5><TextareaAutosize
+              placeholder={defaultText}
+              style={{
+                fontSize: "1.0rem",
+                height: "1vh",
+                width: "8vw",
+                minHeight: "5vh",
+              }}
+              value={text}
+              onChange={handleTextChange} /></> : <h1></h1>}
+            {personBool ? <><Button
+              color="primary"
+              variant="contained"
+              style={{ float: "right" }}
+              onClick={() => {
+                handlePerson();
+              }}
+            >
+              Sort
+            </Button></> : <h1></h1>}
           </h2>
-       {/*</h2> : <h2></h2>} */} 
-        <Stack className={styles.center}>
-          {Object.keys(logList).map((key) => {
-            return (
-              <Card
-                key={key}
-                sx={{
-                  width: "60vw",
-                  backgroundColor: "#1e1e1e",
-                  boxShadow: 1,
-                  p: 1.5,
-                  m: 1,
-                }}
-              >
-                <h2>{key}</h2>
-                {logList[key].map((log) => {
-                  return (
-                    <Grid key={log.hash} container spacing={2}>
-                      <Grid item xs={12} sm container>
+          <Stack className={styles.center}>
+            {Object.keys(logList).map((key) => {
+              return (
+                <Card
+                  key={key}
+                  sx={{
+                    width: "60vw",
+                    backgroundColor: "#1e1e1e",
+                    boxShadow: 1,
+                    p: 1.5,
+                    m: 1,
+                  }}
+                >
+                  <h2></h2>
+                  {logList[key].map((log) => {
+                    return (
+                      <Grid key={log.hash} container spacing={2}>
+                        <Grid item xs={12} sm container>
                         <Grid item xs>
-                          <h3>{log.userid}</h3>
-                        </Grid>
-                        <Grid item xs>
-                          <h3>{log.message}</h3>
-                        </Grid>
-                        <Grid item xs>
-                          <h4>{log.hash}</h4>
-                        </Grid>
-                        <Grid item>
-                          <h5>{log.timeAgo}</h5>
+                            <h4>{log.timestamp2}</h4>
+                          </Grid>
+                          <Grid item xs>
+                            <h3>{log.userid}</h3>
+                          </Grid>
+                          <Grid item xs>
+                            <h3>{log.message}</h3>
+                          </Grid>
+                          <Grid item xs>
+                            <h3>{log.hash}</h3>
+                          </Grid>
+                          <Grid item>
+                            <h3>{log.timeAgo}</h3>
+                          </Grid>
                         </Grid>
                       </Grid>
-                    </Grid>
-                  );
-                })}
-              </Card>
-            );
-          })}
-        </Stack>
-        <Image src={logo} alt="GQL Logo" height={80} objectFit="contain" />
-        <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
-      </Box>
-    )
-  );
-}
+                    );
+                  })}
+                </Card>
+              );
+            })}
+          </Stack>
+          <Image src={logo} alt="GQL Logo" height={80} objectFit="contain" />
+          <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
+        </Box>
+      )
+    );
+  }
 
-export default Log;
+  export default Log;
